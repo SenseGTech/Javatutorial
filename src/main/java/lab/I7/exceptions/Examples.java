eede7e3d5b7f53534976ba815513f29b61142ed3
6thSenSe GoGiCH <sensegdev@gmail.com>
2024-03-13 22:13:28 +0200

    Checked/Unchecked Exceptions / Catching multiple exceptions

--- src/main/java/lab/I7/exceptions/Examples.java
+++ src/main/java/lab/I7/exceptions/Examples.java
@@ -6,8 +6,9 @@ import java.io.FileReader;
 
 public class Examples {
     public static void main(String[] args) {
-       testTryCatch("src/main/java/lab/I7/exceptions/testFile.text");
+       //testTryCatch("src/main/java/lab/I7/exceptions/testFile.text");
         //testUncheckedException();
+        testTryMultipleCatch("George","0");
     }
 
    /* public static void testCheckedException() {
@@ -23,6 +24,19 @@ public class Examples {
             FileReader filereader = new FileReader(filePath);
         } catch (FileNotFoundException e) {
             System.out.println("The file is not found! Please check the file name and path and try again!");
+        }
+    }
+    public static void testTryMultipleCatch (String number,String factor) {
+        try {
+            int parSedNumber = Integer.parseInt(number);
+            int parSedFactor = Integer.parseInt(factor);
+            if (parSedNumber % parSedFactor == 0) {
+                System.out.println(parSedFactor + " is a factor of " + parSedNumber);
+            }
+        } catch(ArithmeticException e){
+            System.out.println("Aritmetic " + e);
+        } catch (NumberFormatException e){
+            System.out.println("Number format Exception " + e);
         }
     }
 }
