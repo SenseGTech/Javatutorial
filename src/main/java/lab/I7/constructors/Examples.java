2c7f3edd0517c11c61500194b5666b8e4fa39412
6thSenSe GoGiCH <sensegdev@gmail.com>
2024-03-13 20:15:05 +0200

    Inside the body of the constructor the values of these three parameters are assigned to the fields of the Empployee object.

--- src/main/java/lab/I7/constructors/Examples.java
+++ src/main/java/lab/I7/constructors/Examples.java
@@ -1,13 +1,16 @@
 package main.java.lab.I7.constructors;
 
+import main.java.lab.I7.constructors.vehicle.Car;
+
 public class Examples {
     public static void main(String[] args) {
         /*Employee george = new Employee("George", "Stankov", 1997);
         Employee ivan = new Employee("Ivan", "Dimitrov", 2000);
         Employee employee = new Employee("George","Stankov");
         /*System.out.println(george);
-        System.out.println(ivan);*/
+        System.out.println(ivan);
         Employee employee = new Employee("George","Stankov");
-        System.out.println(employee);
+        System.out.println(employee);*/
+        Car car = new Car("PB0123","Mercedes");
     }
 }
