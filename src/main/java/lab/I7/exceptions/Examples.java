8d4550fb5f7a250ba277b0fda13f73c552e0d2d1
6thSenSe GoGiCH <sensegdev@gmail.com>
2024-03-13 21:46:04 +0200

    Checked/Unchecked Exceptions

---
+++ src/main/java/lab/I7/exceptions/Examples.java
@@ -0,0 +1,19 @@
+package main.java.lab.I7.exceptions;
+
+import java.io.File;
+import java.io.FileReader;
+
+public class Examples {
+    public static void main(String[] args) {
+        testUncheckedException();
+    }
+
+   /* public static void testCheckedException() {
+        File file = new File("main/java/lab/17/exeptions/testFile.text");
+        FileReader fr = new FileReader(file);
+    }*/
+    public static void testUncheckedException(){
+        int[] num = {1,2,3,4};
+        System.out.println(num[0]);
+    }
+}
